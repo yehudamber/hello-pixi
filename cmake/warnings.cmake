a2382d7e6e752b1568e20a4c76e04adb6f470821
Yehuda Bernáth <yehudamber@gmail.com>
2026-08-16 21:23:32 +0300

    fix: Apply warning flags based on compiler frontend variant

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -9,10 +9,10 @@ function(enable_cxx_warnings target)
     # This ensures the flags are attached based on the compiler used for the target.
     target_compile_options(${target} PRIVATE
         # GCC
-        $<$<CXX_COMPILER_ID:GNU>:-Wall;-Wextra;-Wpedantic;-Wcast-align;-Wshadow;-Wformat=2;-Wlogical-op;-Wdouble-promotion;-Wconversion;-Werror>
+        $<$<CXX_COMPILER_FRONTEND_VARIANT:GNU>:-Wall;-Wextra;-Wpedantic;-Wcast-align;-Wshadow;-Wformat=2;-Wlogical-op;-Wdouble-promotion;-Wconversion;-Werror>
         # Clang
-        $<$<CXX_COMPILER_ID:Clang,AppleClang>:-Wall;-Wextra;-Wpedantic;-Wshadow;-Wformat=2;-Wconversion;-Wdouble-promotion;-Wimplicit-fallthrough;-Werror>
+        $<$<CXX_COMPILER_FRONTEND_VARIANT:Clang,AppleClang>:-Wall;-Wextra;-Wpedantic;-Wshadow;-Wformat=2;-Wconversion;-Wdouble-promotion;-Wimplicit-fallthrough;-Werror>
         # MSVC
-        $<$<CXX_COMPILER_ID:MSVC>:/external:W0;/W4;/w14242;/w14263;/w14265;/w14287;/w14296;/w14545;/w14546;/w14547;/w14549;/w14555;/w14826;/w14905;/w14906;/w14928;/WX>
+        $<$<CXX_COMPILER_FRONTEND_VARIANT:MSVC>:/external:W0;/W4;/w14242;/w14263;/w14265;/w14287;/w14296;/w14545;/w14546;/w14547;/w14549;/w14555;/w14826;/w14905;/w14906;/w14928;/WX>
     )
 endfunction()
